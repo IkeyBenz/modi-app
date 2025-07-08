86953ec1514edd7b80edfe22b1ae37bccb4777a6
IkeyBenz <ikey.benz@gmail.com>
2025-07-08 14:46:51 -0400

    polish

--- functions/src/types.ts
+++ functions/src/types.ts
@@ -18,6 +18,11 @@ export interface ActiveGame extends GameBase {
   roundState: 'pre-deal' | 'playing' | 'tallying';
 }
 
+interface EndedGame extends Omit<ActiveGame, 'status'> {
+  status: 'ended';
+  winner: string;
+}
+
 // Separate document for internal game state (deck, trash)
 export interface GameInternalState {
   deck: CardID[];
@@ -35,4 +40,16 @@ export type CardRank = '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | 'J
 export type CardSuit = 'H' | 'D' | 'C' | 'S';
 export type CardID = `${CardRank}${CardSuit}`;
 
-export type Game = InitialGame | ActiveGame;
+export type Game = InitialGame | ActiveGame | EndedGame;
+
+export function isWaitingForPlayers(game: Game): game is InitialGame {
+  return game.status === 'gathering-players';
+}
+
+export function isActive(game: Game): game is ActiveGame {
+  return game.status === 'active';
+}
+
+export function isEnded(game: Game): game is EndedGame {
+  return game.status === 'ended';
+}
